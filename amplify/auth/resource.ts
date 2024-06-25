9a4de13e57a3ca85d777cf0f371ba6a5e3c9694f
AhmetAhunbayAWS <ahmetahu@amazon.com>
2024-06-25 09:28:25 -0700

    adds back files

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,19 +8,20 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
     externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'), 
+        scopes: ['email'],
+      },
       oidc: [
         {
           name: "OktaClient",
           clientId: secret('OKTA_CLIENT_ID'),
           clientSecret: secret('OKTA_CLIENT_SECRET'),
           issuerUrl: 'https://dev-z0pet31ai0jjaayy.us.auth0.com',
+          scopes: ['openid','email']
         }
       ],
-      google: {
-        clientId: secret('GOOGLE_CLIENT_ID'),
-        clientSecret: secret('GOOGLE_CLIENT_SECRET'), 
-        scopes: ['email'],
-      },
       callbackUrls: ['http://localhost:5173/profile', 'https://main.d21e0xd5en76i9.amplifyapp.com/profile'],
       logoutUrls:['http://localhost:5173/', 'https://main.d21e0xd5en76i9.amplifyapp.com']
     }
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+
 import { Authenticator } from '@aws-amplify/ui-react'
 import useIsSignedIn from './CustomHook'
 import Todo from './Todo'
 import '@aws-amplify/ui-react/styles.css'
 import OktaSignIn from './okta'
+
 function App() {
 
   console.log("calling useIsSignedIn")
   const signed = useIsSignedIn();
   console.log(signed)
+  const auth = <Authenticator/>
+  console.log(auth)
+
   return signed ? <Todo/> : 
   <div>
-    <Authenticator/>
+    {auth}
     <OktaSignIn/>
   </div>
 
